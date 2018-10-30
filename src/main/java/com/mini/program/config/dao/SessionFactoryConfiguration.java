ace0d7393c31005c5fd795c164b5bf907171373b
周冰洁 <zhoubingjie@allinmd.cn>
2018-10-30 10:27:08 +0800

    优化

--- src/main/java/com/mini/program/config/dao/SessionFactoryConfiguration.java
+++ src/main/java/com/mini/program/config/dao/SessionFactoryConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 
 import javax.sql.DataSource;
@@ -18,29 +19,82 @@ import java.io.IOException;
 
 @Configuration
 public class SessionFactoryConfiguration {
+
+    // mybatis-config.xml配置文件的路径
+    private static String mybatisConfigFile;
+
     @Value("${mybatis_config_file}")
-    private String mybatisConfigFilePath;
+    public void setMybatisConfigFile(String mybatisConfigFile) {
+        SessionFactoryConfiguration.mybatisConfigFile = mybatisConfigFile;
+    }
+
+    // mybatis mapper文件所在路径
+    private static String mapperPath;
 
     @Value("${mapper_path}")
-    private String mapperPath;
+    public void setMapperPath(String mapperPath) {
+        SessionFactoryConfiguration.mapperPath = mapperPath;
+    }
+
+    // 实体类所在的package
+    @Value("${type_alias_package}")
+    private String typeAliasPackage;
 
     @Autowired
     private DataSource dataSource;
 
-    @Value("${type_alias_package}")
-    private String entityPackage;
-
+    /**
+     * 创建sqlSessionFactoryBean 实例 并且设置configtion 设置mapper 映射路径 设置datasource数据源
+     *
+     * @return
+     * @throws IOException
+     */
     @Bean(name = "sqlSessionFactory")
     public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath));
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + mapperPath;
-
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+        // 设置mybatis configuration 扫描路径
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFile));
+        // 添加mapper 扫描路径
+        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + mapperPath;
+        sqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources(packageSearchPath));
+        // 设置dataSource
         sqlSessionFactoryBean.setDataSource(dataSource);
-        sqlSessionFactoryBean.setTypeAliasesPackage(entityPackage);
-        return sqlSessionFactoryBean ;
+        // 设置typeAlias 包扫描路径
+        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasPackage);
+        return sqlSessionFactoryBean;
     }
+
+
+
+
+
+
+//
+//
+//    @Value("${mybatis_config_file}")
+//    private  String mybatisConfigFilePath;
+//
+//    @Value("${mapper_path}")
+//    private String mapperPath;
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Value("${type_alias_package}")
+//    private String entityPackage;
+//
+//    @Bean(name = "sqlSessionFactory")
+//    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath));
+//
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + mapperPath;
+//
+//        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+//        sqlSessionFactoryBean.setDataSource(dataSource);
+//        sqlSessionFactoryBean.setTypeAliasesPackage(entityPackage);
+//        return sqlSessionFactoryBean ;
+//    }
 }
